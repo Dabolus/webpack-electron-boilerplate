35822f468c6ed789f120090219bc9c357cbc6855
Giorgio Garasto <giorgio@garasto.it>
2018-11-04 09:41:02 +0100

    chore: Temporarily disable unused Copy Webpack Plugin

--- webpack/renderer.base.config.ts
+++ webpack/renderer.base.config.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:object-literal-sort-keys */
 /// <reference types="../typings" />
-import CopyPlugin from 'copy-webpack-plugin';
+// import CopyPlugin from 'copy-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { resolve } from 'path';
 import ScriptExtHtmlPlugin from 'script-ext-html-webpack-plugin';
@@ -10,13 +10,13 @@ import baseConfig from './base.config';
 
 const config: Configuration = smartMerge(baseConfig, {
   plugins: [
-    new CopyPlugin([
+    /* new CopyPlugin([
       // Assets
       {
         from: resolve(__dirname, '../src/assets'),
         to: '.',
       },
-    ]),
+    ]), */
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
       chunkFilename: 'styles/[id].css',
