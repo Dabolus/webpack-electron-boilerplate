29061086424737e9ee1faca240677d8c3342d0a1
Giorgio Garasto <giorgio@garasto.it>
2018-11-04 09:41:56 +0100

    feature: Improve dev configuration

--- webpack/main.dev.config.ts
+++ webpack/main.dev.config.ts
@@ -7,11 +7,17 @@ import baseConfig from './base.config';
 const config: Configuration = smartMerge(baseConfig, {
   mode: 'development',
   target: 'electron-main',
+  devtool: 'eval-source-map',
   entry: resolve(__dirname, '../src/main/index'),
   output: {
     filename: 'main.js',
     path: resolve(__dirname, '../node_modules/.build'),
     pathinfo: false,
+  },
+  optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
   },
 });
 
